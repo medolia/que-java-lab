d9358fb0f8a356735390ce76b34ea4ca98f420e2
lbli <lbli@trip.com>
2021-11-01 13:01:20 +0800

    big merge

--- rpc-in-action/src/main/java/com/meodlia/lab/rpc/registry/Registry.java
+++
@@ -1,8 +0,0 @@
-package com.meodlia.lab.rpc.registry;
-
-/**
- * @author lbli
- * @date 2021/10/2
- */
-public interface Registry {
-}
---
+++ spring-boot-demo/src/main/java/com/medolia/spring/demo/uniResult/UniExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.medolia.spring.demo.uniResult;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @author lbli
+ * @date 2021/10/1
+ */
+@Slf4j
+@RestControllerAdvice
+public class UniExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResultData<String> handle(Exception e) {
+        log.warn("发生异常: {}", e.getMessage(), e);
+        return ResultData.fail(ReturnCode.RC500.getCode(), e.getMessage());
+    }
+}
