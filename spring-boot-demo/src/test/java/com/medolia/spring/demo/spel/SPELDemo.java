d1fa717525cef1f0b5a18e476fd9f78eb3e4e4e7
melodia <a458972801@gmail.com>
2022-06-04 03:11:06 +0800

    spel redis yml

---
+++ spring-boot-demo/src/test/java/com/medolia/spring/demo/spel/SPELDemo.java
@@ -0,0 +1,34 @@
+package com.medolia.spring.demo.spel;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.expression.Expression;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+
+import java.util.Objects;
+
+/**
+ * @author lbli
+ * @date 2022/5/27
+ */
+@Slf4j
+public class SPELDemo {
+
+    @Test
+    void demo() {
+        SpelExpressionParser parser = new SpelExpressionParser();
+        Expression expression = parser.parseExpression("#root.purchaseName");
+        Order order = new Order();
+        order.setPurchaseName("medolia");
+        Object value = expression.getValue(order);
+        log.info(Objects.requireNonNull(value).toString());
+    }
+
+    @Data
+    private class Order {
+
+        private String purchaseName;
+
+    }
+}
